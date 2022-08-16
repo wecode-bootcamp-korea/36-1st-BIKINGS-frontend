f661ef83118d8ce03c9affb909282a4c7b229654
CodyMan0 <hys83751952@gmail.com>
2022-08-16 20:00:56 +0900

    진짜 초기 세팅 완료입니다..

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import ReactDOM from 'react-dom/client';
 import { Router } from 'react-router';
 import './styles/reset.scss';
+import './styles/common.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Router />);
