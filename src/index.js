27b0cddfaa1d7660d1998fa55e9e5aaa2f9f5d0a
CodyMan0 <hys83751952@gmail.com>
2022-08-16 17:43:31 +0900

    BIKING-Fronted 초기 세팅 완료

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Router } from 'react-router';
+import './styles/reset.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Router />);
