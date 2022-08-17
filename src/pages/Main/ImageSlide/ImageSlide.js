60334e3659cbb21853e0043934afa37e8a6bb1e2
kwon young jun <yonng0000@naver.com>
2022-08-17 15:20:03 +0900

    ADD: 버튼 클릭시 image Change 기능 구현 Function.js 추가

---
+++ src/pages/Main/ImageSlide/ImageSlide.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { image } from '../../../Function';
+import './ImageSlide.scss';
+
+const ImageSlide = () => {
+  const [current, setCurrent] = useState(0);
+  const length = IMG_BOX.length;
+
+  const showNextImage = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const showprevImage = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  return (
+    <div className="ImageSlide">
+      <img
+        className="btn left"
+        src={image('left-arrow')}
+        alt="leftArrrow"
+        onClick={showprevImage}
+      />
+      {IMG_BOX.map((picture, index) => {
+        return (
+          <div
+            className={index === current ? 'slide_active' : 'slide'}
+            key={picture.id}
+          >
+            {index === current && (
+              <img className="imgBox" src={picture.src} alt="slideImage" />
+            )}
+          </div>
+        );
+      })}
+      <img
+        className="btn right"
+        src={image('right-arrow')}
+        alt="rightArrrow"
+        onClick={showNextImage}
+      />
+    </div>
+  );
+};
+
+export default ImageSlide;
+
+const IMG_BOX = [
+  {
+    id: 1,
+    src: image('MainImages/mainImg1'),
+  },
+  {
+    id: 2,
+    src: image('MainImages/mainImg2'),
+  },
+  {
+    id: 3,
+    src: image('MainImages/mainImg3'),
+  },
+  {
+    id: 4,
+    src: image('MainImages/mainImg4'),
+  },
+];
