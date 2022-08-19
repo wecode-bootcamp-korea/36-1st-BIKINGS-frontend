b9ea974e40715f3a2602e40f61039ebb55ddee4a
CodyMan0 <hys83751952@gmail.com>
2022-08-19 09:48:51 +0900

    장바구니 모달창 UI 및 기능 구현 완료

--- src/pages/ProductDetail/Basket/Basket.js
+++ src/pages/ProductDetail/Basket/Basket.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './Basket.scss';
 
-const Basket = () => {
+const Basket = ({ setShowModal }) => {
   return (
     <div className="basket">
       <div className="basketContainer">
         <div className="firstBasket">
-          <div>오던세 라고아 2인조 13피스</div>
-          <div className="subDes">적립제외상품</div>
+          <p>오던세 라고아 2인조 13피스</p>
+          <p className="subDes">적립제외상품</p>
           <span className="price">149,250</span>
           <span className="subPrice">199,250</span>
         </div>
         <div className="secondBasket">
-          <span>합계</span>
+          <p>합계</p>
           <span className="bigPrice">149,250원</span>
         </div>
         <div className="secondHalfBasket">
@@ -20,8 +20,12 @@ const Basket = () => {
           <span className="">로그인 후, 회원할인가 제공</span>
         </div>
         <div className="thirdBasket">
-          <button className="btnOne">취소</button>
-          <button className="btnTwo">장바구니 담기</button>
+          <button onClick={() => setShowModal(false)} className="btnOne">
+            취소
+          </button>
+          <button onClick={() => setShowModal(false)} className="btnTwo">
+            장바구니 담기
+          </button>
         </div>
       </div>
     </div>
--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Basket from './Basket/Basket';
 import './ProductDetail.scss';
 
 const ProductDetail = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="productDetail">
-      <Basket />
-      <div className="basketBtn">
+      {showModal ? <Basket setShowModal={setShowModal} /> : null}
+      <button
+        onClick={() => setShowModal(true)}
+        className="basketBtn"
+        type="button"
+      >
         <img className="basketImg" src="/images/카트.png" alt="cart" />
-      </div>
+      </button>
     </div>
   );
 };
