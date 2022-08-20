3a319e41244bfc86683e9877c75dcf6fa50d4307
kwon young jun <yonng0000@naver.com>
2022-08-20 14:31:09 +0900

    Add topBtn 기능 UI 완성

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Nav from './components/Nav/Nav';
+import Topbtn from './components/TopBtn/TopBtn';
 import Main from './pages/Main/Main';
 
 const Router = () => {
@@ -11,6 +12,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
+      <Topbtn />
       <Footer />
     </BrowserRouter>
   );
