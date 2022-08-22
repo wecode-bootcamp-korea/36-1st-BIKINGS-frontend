eb424fadb548fee06c1b9a7416cf66b6ea8b0eef
CodyMan0 <hys83751952@gmail.com>
2022-08-22 21:09:19 +0900

    ADD: 장바구니 데이터 통신 준비 완료

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import './ProductDetail.scss';
 import Tags from './Tags/Tags';
+import './ProductDetail.scss';
 
 const ProductDetail = () => {
   const [data, setData] = useState([]);
@@ -13,38 +13,36 @@ const ProductDetail = () => {
       .then(response => response.json())
       .then(result => setData(result));
   }, [userId]);
-  // useEffect 쓴 이유 안썼을때 무한으로 받아져서
-  // const tagData = data.map(data => data.tags);
-  // console.log(tagData);
 
-  //useParams를 쓰면 state를 못받아오는 현상
   return (
     <div className="productDetail">
       <div className="bigTitle">
-        <h1>{data.map(data => data.name)}</h1>
+        <h1>{data.length > 0 && data.map(data => data.name)}</h1>
       </div>
       <div className="priceSection">
         <strong className="price">
           <span className="recommendPrice">권장소비자가격</span>
-          {data.map(data => Number(data.price).toLocaleString()) + '원'}
+          {data.length > 0 &&
+            data.map(data => Number(data.price).toLocaleString()) + '원'}
         </strong>
         <ul className="hashTagContainer">
-          {data.map((data, idx) => (
-            <Tags key={idx} data={data} />
-          ))}
+          {data.length > 0 &&
+            data[0].tags.map((data, idx) => <Tags key={idx} tags={data} />)}
         </ul>
         <div className="snsContainer">
           <a className="sns" href="https://www.instagram.com">
-            facebook
+            <i className="fa-brands fa-facebook-f icon" />
           </a>
           <a className="sns" href="https://www.instagram.com">
-            instagram
+            <i class="fa-brands fa-instagram icon" />
           </a>
         </div>
       </div>
 
       <div className="detailImgSection">
-        <img src={data.map(data => data.detail_image_url)} alt="images" />
+        {data.length > 0 && (
+          <img src={data.map(data => data.detail_image_url)} alt="images" />
+        )}
       </div>
     </div>
   );
--- src/pages/ProductDetail/Tags/Tags.js
+++ src/pages/ProductDetail/Tags/Tags.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import './Tags.scss';
 
-const Tags = ({ data }) => {
-  const { id, tags } = data;
-
-  return (
-    <div className="tags">
-      <li key={id} className="hashTag" />
-      {tags}
-    </div>
-  );
+const Tags = ({ tags }) => {
+  return <li className="hashTag">{tags}</li>;
 };
 
 export default Tags;
