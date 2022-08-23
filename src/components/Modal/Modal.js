29625f0eaf1e8bf8332946bc357c220bb6df4536
kwon young jun <yonng0000@naver.com>
2022-08-23 15:38:29 +0900

    ADD modal UI

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import './Modal.scss';
+
+const Modal = ({ togleModal, content, propsFuntion }) => {
+  const closeModal = e => {
+    if (e.target === e.currentTarget) {
+      togleModal();
+    }
+  };
+
+  useEffect(() => {
+    document.body.style = `overflow: hidden`;
+    return () => (document.body.style = `overflow: auto`);
+  }, []);
+
+  return (
+    // <div>
+    //   {showModal && (
+    <div className="modal" onClick={closeModal}>
+      <div className="modalContainer">
+        <p className="content">{content}</p>
+        <div className="modalBtn">
+          <button className="btn close" onClick={togleModal}>
+            닫기
+          </button>
+          <button
+            className="confirm btn"
+            onClick={function () {
+              propsFuntion();
+              togleModal();
+            }}
+          >
+            확인
+          </button>
+        </div>
+      </div>
+    </div>
+    //   )}
+    // </div>
+  );
+};
+
+export default Modal;
