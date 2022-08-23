a45a7b4603d563564d071c11c4f1703a466f7703
MoonHayden <hdmoon127@gmail.com>
2022-08-23 23:00:59 +0900

    MyPage UI수정 및 기능추가

--- src/pages/Mypage/UserInfo.js
+++ src/pages/Mypage/UserInfo.js
@@ -10,26 +10,30 @@ const UserInfo = ({ info }) => {
   };
 
   return (
-    <>
-      <div className="userInfo">
-        <div className="id">ID : {info.username} </div>
-        <div className="nameContactBirth">이름 : {info.name}</div>
-        <div className="nameContactBirth">전화번호 : {info.contact}</div>
-        <div className="nameContactBirth">생년월일 : {info.birth}</div>
+    <div className="userInfo">
+      <p>{info.name}님, 반가워요!</p>
+      <div className="userInfoContainer">
+        <div className="userInfoDetail">
+          <p className="id">ID : {info.username} </p>
+          {/* <div className="nameContactBirth">이름 : {info.name}</div> */}
+          <p className="nameContactBirth">전화번호 : {info.contact}</p>
+          <p className="nameContactBirth">생년월일 : {info.birth}</p>
+          <ol className="userOption">
+            <li className="userInfoChange">회원정보 변경</li>
+            <li className="userInfoDelete" onClick={openModal}>
+              회원 탈퇴
+            </li>
+          </ol>
+        </div>
+
+        <img
+          className="bikeMyPage"
+          alt="bikeMyPage"
+          src="images/paul-green-gOHfFgwyDNM-unsplash.jpg"
+        />
+        {showModal && <DeleteId setShowModal={setShowModal} />}
       </div>
-      <ol className="userOption">
-        <li className="userInfoChange">회원정보 변경</li>
-        <li className="userInfoDelete" onClick={openModal}>
-          회원 탈퇴
-        </li>
-      </ol>
-      <img
-        className="bikeMyPage"
-        alt="bikeMyPage"
-        src="images/paul-green-gOHfFgwyDNM-unsplash.jpg"
-      />
-      {showModal && <DeleteId setShowModal={setShowModal} />}
-    </>
+    </div>
   );
 };
 
