0b7ff45251fad73b14aefb054ac5bdef1a79756a
kwon young jun <yonng0000@naver.com>
2022-08-24 10:27:19 +0900

    modify 양식수정

--- src/config.js
+++ src/config.js
@@ -38,19 +38,16 @@ export const fetchData = async (url, setState) => {
           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImhlbGxvMUBnbWFpbC5jb20iLCJpZCI6MSwiYmlydGgiOiIyMDAwLTA5LTMwVDE1OjAwOjAwLjAwMFoiLCJjb250YWN0IjoiMDEwLTU1NTUtNDQ0NCIsInBvaW50IjoxMDAwMDAwMCwibmFtZSI6ImxlZSIsImlhdCI6MTY2MTEzMDIzNX0.1ZQ9uebdi1950j-dhZcG-3Tsf_9KTjWgFltGYQ7WOVk',
       },
     });
-    if (!response.ok) {
-      throw new Error('서버가 이상합니다.');
-    }
     const data = await response.json();
     setState(Object.values(data)[1]);
-  } catch (error) {
-    throw new Error(`에러가 발생했습니다. ${error.message}`);
+  } catch (err) {
+    throw new Error(err);
   }
 };
 
 export const deleteData = async (url, id, execution) => {
   try {
-    const response = await fetch(url, {
+    await fetch(url, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
@@ -59,10 +56,7 @@ export const deleteData = async (url, id, execution) => {
       },
       body: JSON.stringify(id),
     }).then(execution);
-    if (!response.ok) {
-      throw new Error('서버가 이상합니다.');
-    }
-  } catch (error) {
-    throw new Error(`에러가 발생했습니다. ${error.message}`);
+  } catch (err) {
+    throw new Error(err);
   }
 };
