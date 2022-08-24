58d351beaddf0d19828b8ec9699a6f9adf269b61
CodyMan0 <hys83751952@gmail.com>
2022-08-24 19:18:54 +0900

    fix conflict done

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -28,45 +28,48 @@ const ProductDetail = () => {
 
   return (
     <div className="productDetail">
-      {showModal && (
-        <Carts
-          setShowModal={setShowModal}
-          setProductData={setProductData}
-          getData={getData}
-          productData={productData}
-          closeModal={closeModal}
-        />
-      )}
-      <button onClick={closeModal} className="cartsBtn" type="button">
-        <img className="cartsImg" src="/images/카트.png" alt="cart" />
-      </button>
-      <div className="bigTitle">
-        <h1>{data.length > 0 && data.map(data => data.name)}</h1>
-      </div>
-      <div className="priceSection">
-        <strong className="price">
-          <span className="recommendPrice">권장소비자가격</span>
-          {data.length > 0 &&
-            data.map(data => Number(data.price).toLocaleString()) + '원'}
-        </strong>
-        <ul className="hashTagContainer">
-          {data.length > 0 &&
-            data[0].tags.map((data, idx) => <Tags key={idx} tags={data} />)}
-        </ul>
-        <div className="snsContainer">
-          <a className="sns" href="https://www.instagram.com">
-            <i className="fa-brands fa-facebook-f icon" />
-          </a>
-          <a className="sns" href="https://www.instagram.com">
-            <i className="fa-brands fa-instagram icon" />
-          </a>
-        </div>
-      </div>
-
-      <div className="detailImgSection">
-        {data.length > 0 && (
-          <img src={data.map(data => data.detail_image_url)} alt="images" />
+      <div className="nav1" />
+      <div className="productDetailContainer">
+        {showModal && (
+          <Carts
+            setShowModal={setShowModal}
+            setProductData={setProductData}
+            getData={getData}
+            productData={productData}
+            closeModal={closeModal}
+          />
         )}
+        <button onClick={closeModal} className="cartsBtn" type="button">
+          <img className="cartsImg" src="/images/카트.png" alt="cart" />
+        </button>
+        <div className="bigTitle">
+          <h1>{data.length > 0 && data.map(data => data.name)}</h1>
+        </div>
+        <div className="priceSection">
+          <strong className="price">
+            <span className="recommendPrice">권장소비자가격</span>
+            {data.length > 0 &&
+              data.map(data => Number(data.price).toLocaleString()) + '원'}
+          </strong>
+          <ul className="hashTagContainer">
+            {data.length > 0 &&
+              data[0].tags.map((data, idx) => <Tags key={idx} tags={data} />)}
+          </ul>
+          <div className="snsContainer">
+            <a className="sns" href="https://www.instagram.com">
+              <i className="fa-brands fa-facebook-f icon" />
+            </a>
+            <a className="sns" href="https://www.instagram.com">
+              <i className="fa-brands fa-instagram icon" />
+            </a>
+          </div>
+        </div>
+
+        <div className="detailImgSection">
+          {data.length > 0 && (
+            <img src={data.map(data => data.detail_image_url)} alt="images" />
+          )}
+        </div>
       </div>
     </div>
   );
