d9666463101be7e521e3ba6cd4c651ed029b3b48
kwon young jun <yonng0000@naver.com>
2022-08-24 10:25:30 +0900

    modify 가격 양식 변경

--- src/pages/Product/BycleInfo/BycleInfo.js
+++ src/pages/Product/BycleInfo/BycleInfo.js
@@ -7,7 +7,9 @@ const BycleInfo = ({ bycle: { name, price, cover_image_url, tags } }) => {
       <img className="bycleImg" src={cover_image_url} alt="bycleImg" />
       <div className="bycleContent">
         <div className="bycleName">{name}</div>
-        <div className="byclePrice">{Number(price)}</div>
+        <div className="byclePrice">
+          {Number(price).toLocaleString() + '원'}
+        </div>
         <div className="bycleHashtag">
           {tags.map((tag, i) => (
             <button className="hashTag" key={i}>
